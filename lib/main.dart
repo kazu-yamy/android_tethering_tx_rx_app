40ee878cbd74574e695cc9936d2d7868f138a9c4
kazu-yamy <nshtk0125@gmail.com>
2022-07-07 23:25:49 +0900

    IP Address State
    Create IP Address State Function

--- lib/main.dart
+++ lib/main.dart
@@ -29,13 +29,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              '$_counter',
+              'aaaaa',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
