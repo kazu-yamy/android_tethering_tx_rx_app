c53e1f3f3f00baa469b4bac2ba25d7f1e8a5b45b
kazu-yamy <nshtk0125@gmail.com>
2022-07-08 16:51:25 +0900

    create IP State widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
   runApp(const MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -11,17 +9,12 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-
   Widget build(BuildContext context) {
 
     return MaterialApp(
-
       title: 'Flutter Demo',
-
       theme: ThemeData.dark(),
-
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-
     );
 
   }
@@ -35,7 +28,6 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-
   State<MyHomePage> createState() => _MyHomePageState();
 
 }
@@ -43,15 +35,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   @override
-
   Widget build(BuildContext context) {
 
     return Scaffold(
 
       appBar: AppBar(
-
         title: Text(widget.title),
-
       ),
 
       body: Center(
@@ -68,11 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
 
         ),
-
       ),
-
     );
-
   }
-
 }
