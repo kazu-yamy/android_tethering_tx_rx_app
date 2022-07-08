c1b67d27d2498c00ea98e99b79ac6b1d1ff0aa1b
kazu-yamy <nshtk0125@gmail.com>
2022-07-08 15:18:32 +0900

    prepare

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,78 @@
 import 'package:flutter/material.dart';
 
 void main() {
+
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({Key? key}) : super(key: key);
 
   @override
+
   Widget build(BuildContext context) {
+
     return MaterialApp(
+
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+
+      theme: ThemeData.dark(),
+
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
     );
+
   }
+
 }
 
 class MyHomePage extends StatefulWidget {
+
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
+
   State<MyHomePage> createState() => _MyHomePageState();
+
 }
 
 class _MyHomePageState extends State<MyHomePage> {
 
   @override
+
   Widget build(BuildContext context) {
+
     return Scaffold(
+
       appBar: AppBar(
+
         title: Text(widget.title),
+
       ),
+
       body: Center(
+
         child: Column(
+
           mainAxisAlignment: MainAxisAlignment.center,
+
           children: <Widget>[
             Text(
               'aaaaa',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
+
         ),
+
       ),
+
     );
+
   }
+
 }
